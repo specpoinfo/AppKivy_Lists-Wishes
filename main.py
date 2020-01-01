b69e9c217af3f58b5316165902129c8da149e7d7
specpoinfo <subbotin1818@gmail.com>
2020-01-01 23:40:13 +0300

    New file for easier settings with Layouts

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 
+
 class AuthApp(App):
     def build(self):
-        gl = GridLayout()
-        btn = Button(text='Привет')
+        pass
 
-        gl.add_widget(btn)
-        return gl
+    def change_name(self, text):
+        pass
 
 if __name__ == "__main__":
     AuthApp().run()
